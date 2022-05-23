8fa346007d0c65015332c1c1e8612d8bfe6a9016
Abhaysingh4321 <94883207+AbhaySingh4321@users.noreply.github.com>
2022-05-23 04:12:45 +0000

    9

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -6,14 +6,12 @@ fh = open(fname)
 lst = list()
 
 for line in fh:
-    
-        for word in line:
-            word=line.rstrip().split()
-            if element in line:
+    word=line.rstrip().split()
+    for element in word:
+        if element in lst:
                 continue
-            else :
+        else :
                 lst.append(element)
-               
 
-print(sort(lst))        
-
+l=lst.sort()
+print(l)
