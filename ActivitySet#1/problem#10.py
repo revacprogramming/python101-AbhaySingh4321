9ec1a2ab0aec3ae228229b5548ab1121c6834680
Abhaysingh4321 <94883207+AbhaySingh4321@users.noreply.github.com>
2022-05-30 04:28:54 +0000

    10

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,4 +1,22 @@
 # Dictionaries
-
-filename = "dataset/mbox-short.txt"
-f
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+di=dict()
+for lin in handle:
+    lin = lin.rstrip()
+    wds = lin.split()
+    for w in wds:
+        if w in di:
+            di[w] = di[w] + 1
+        else:
+            di[w] = 1
+            
+largest=-1
+theword=None
+for k,v in di.items():
+    if v > largest:
+        largest = v
+        theword = k
+print(theword,largest)        
